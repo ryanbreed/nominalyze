e11a4abca204dea21f5c6a83f84b76e02447ddd5
Ryan Breed <opensource@breed.org>
2012-01-21 08:48:14 -0600

    reorganization

--- lib/nominalyze.rb
+++ lib/nominalyze.rb
@@ -1,3 +1,12 @@
+unless File.respond_to? :realpath
+  class File #:nodoc:
+    def self.realpath path
+      return realpath(File.readlink(path)) if symlink?(path)
+      path
+    end
+  end
+end
+$: << File.expand_path(File.dirname(File.realpath(__FILE__)) + '/nominalyze')
 
 require 'java'
 require 'rubygems'
@@ -9,6 +18,7 @@ require 'dnsjava-2.1.3.jar'
 require 'dns_message'
 require 'udp_frame'
 require 'dns_parser'
+require 'json'
 
 module Nominalyze
   VERSION="0.5.0"
